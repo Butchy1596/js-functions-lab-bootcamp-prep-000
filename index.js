b4fa8ba72ce9be1a01d1488d08d316ef03c101c9
Butchy1596 <dterra0726@gmail.com>
2018-03-02 02:16:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(name){
+/*function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 
@@ -11,18 +11,15 @@ function happyHolidaysTo(holiday, name){
   return `Happy ${holiday}, ${name}!`;
 }
 
-
-/*+
-+function happyHolidaysTo(name) {
-+  return `Happy holidays, ${name}!`;
-+}
-+
-+function happyHolidayTo(holiday, name) {
-+  return `Happy ${holiday}, ${name}!`;
-+}
-+
-+function holidayCountdown(holiday, days) {
-+  return `It\'s ${days} days until ${holiday}!`;
-+}
-
 */
+
+function happyHolidaysTo(name) {
+ return `Happy holidays, ${name}!`;
+}
+function happyHolidayTo(holiday, name) {
+ return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`;
+}

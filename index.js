3f07144cda150434b2f85bbcd9f2d264976f7655
Butchy1596 <dterra0726@gmail.com>
2018-03-02 01:25:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
-/*function happyHolidays(){
+function happyHolidays(){
   return ("Happy holidays!");
 }
-*/
 
+var name = "Dominick"
 function happyHolidaysTo(name){
   return ("Happy holidays," + name "!");
 }
